d048db0d3b8729767a113220cd3d5ab38b171792
fellipe-ribeiro <felliperibeiro2@hotmail.com>
2021-02-07 01:27:15 -0300

    version 0.1.3

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback } from 'react';
 import { StatusBar } from 'react-native';
 
 import {
@@ -13,12 +13,39 @@ import {
   CategoriesList,
   CategoriesContainer,
   CategoriesImg,
+  CategoriesTitleContainer,
+  CategoriesTitle,
+  ContainerRestaurants,
+  RestaurantsText,
+  ExpandButton,
+  ExpandButtonText,
+  RestaurantsContainer,
+  RestaurantsList,
+  RestaurantsImg,
+  RestaurantsTitle,
+  RestaurantsTitleContainer,
+  RatingRestaurantContainer,
+  RatingRestaurantIcon,
+  RatingRestaurantNumber,
+  PopularContainer,
+  PopularTitle,
+  ExpandButtonPopular,
+  ExpandButtonPopularText,
+  PopularRestaurantsContainer,
+  PopularRestaurantsImg,
+  PopularRestaurantsList,
+  PopularRestaurantsTitle,
+  PopularRestaurantsTitleContainer,
+  PopularRatingRestaurantContainer,
+  PopularRatingRestaurantDescription,
+  PopularRatingRestaurantIcon,
 } from './styles';
 
 import { InputSearch } from '../../components/Input';
 
 import cartImg from '../../assets/shopping-cart.png';
 import iconLocalizationImg from '../../assets/Grupo6819.png';
+import ratingImg from '../../assets/Caminho8560.png';
 
 import img1 from '../../assets/photos/photo-1.png';
 import img2 from '../../assets/photos/photo-2.png';
@@ -32,11 +59,66 @@ import img9 from '../../assets/photos/photo-9.png';
 
 export interface ICategories {
   img: any;
-  name: string;
+  name: any;
+}
+
+export interface IRestaurants {
+  img: any;
+  name: any;
 }
 
 const Dashboard: React.FC = () => {
   const categories: ICategories[] = [img1, img2, img3, img4];
+  const restaurants: IRestaurants[] = [img5, img6, img7];
+
+  const handleCategory = useCallback(category => {
+    let title = '';
+    switch (category) {
+      case 11:
+        title = 'Ofertas';
+        return title;
+      case 12:
+        title = 'Almoço';
+        return title;
+      case 13:
+        title = 'Italiana';
+        return title;
+
+      default:
+        title = 'Indiana';
+        return title;
+    }
+  }, []);
+
+  const handleRestaurant = useCallback(restaurant => {
+    let title = '';
+    switch (restaurant) {
+      case 15:
+        title = 'Mama Pizza';
+        return title;
+      case 16:
+        title = 'Café de Noir';
+        return title;
+      default:
+        title = 'Panificadora Bella Manhã';
+        return title;
+    }
+  }, []);
+
+  const handleRatingNumber = useCallback(restaurant => {
+    let description = '';
+    switch (restaurant) {
+      case 15:
+        description = '(124 avaliações) Pizza Itália';
+        return description;
+      case 16:
+        description = '(124 avaliações) Café';
+        return description;
+      default:
+        description = '(124 avaliações) Café Padaria';
+        return description;
+    }
+  }, []);
 
   return (
     <>
@@ -64,12 +146,84 @@ const Dashboard: React.FC = () => {
             horizontal
             showsHorizontalScrollIndicator={false}
             data={categories}
-            keyExtractor={categorie => String(categorie)}
-            renderItem={({ item: categorie }) => (
-              <CategoriesImg source={categorie} />
+            keyExtractor={category => String(category)}
+            renderItem={({ item: category }) => (
+              <>
+                <CategoriesImg source={category} />
+                <CategoriesTitleContainer>
+                  <CategoriesTitle>{handleCategory(category)}</CategoriesTitle>
+                </CategoriesTitleContainer>
+              </>
             )}
           />
         </CategoriesContainer>
+
+        <ContainerRestaurants>
+          <RestaurantsText>Restaurantes mais populares</RestaurantsText>
+
+          <ExpandButton>
+            <ExpandButtonText>Ver tudo</ExpandButtonText>
+          </ExpandButton>
+        </ContainerRestaurants>
+
+        <RestaurantsContainer onPress={() => {}}>
+          <RestaurantsList
+            showsHorizontalScrollIndicator={false}
+            data={restaurants}
+            keyExtractor={restaurant => String(restaurant)}
+            renderItem={({ item: restaurant }) => (
+              <>
+                <RestaurantsImg source={restaurant} />
+                <RestaurantsTitleContainer>
+                  <RestaurantsTitle>
+                    {handleRestaurant(restaurant)}
+                  </RestaurantsTitle>
+                </RestaurantsTitleContainer>
+
+                <RatingRestaurantContainer>
+                  <RatingRestaurantIcon source={ratingImg} />
+                  <RatingRestaurantNumber>
+                    {handleRatingNumber(restaurant)}
+                  </RatingRestaurantNumber>
+                </RatingRestaurantContainer>
+              </>
+            )}
+          />
+        </RestaurantsContainer>
+
+        <PopularContainer>
+          <PopularTitle>Mais populares</PopularTitle>
+
+          <ExpandButtonPopular>
+            <ExpandButtonPopularText>Ver todos</ExpandButtonPopularText>
+          </ExpandButtonPopular>
+        </PopularContainer>
+
+        <PopularRestaurantsContainer onPress={() => {}}>
+          <PopularRestaurantsList
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            data={restaurants}
+            keyExtractor={restaurant => String(restaurant)}
+            renderItem={({ item: restaurant }) => (
+              <>
+                <PopularRestaurantsImg source={restaurant} />
+                <PopularRestaurantsTitleContainer>
+                  <PopularRestaurantsTitle>
+                    {handleRestaurant(restaurant)}
+                  </PopularRestaurantsTitle>
+
+                  <PopularRatingRestaurantContainer>
+                    <PopularRatingRestaurantDescription>
+                      Pizza Italiana
+                    </PopularRatingRestaurantDescription>
+                    <PopularRatingRestaurantIcon source={ratingImg} />
+                  </PopularRatingRestaurantContainer>
+                </PopularRestaurantsTitleContainer>
+              </>
+            )}
+          />
+        </PopularRestaurantsContainer>
       </Container>
     </>
   );
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components/native';
 import { FlatList } from 'react-native';
-import { ICategories } from './index';
+import { RectButton } from 'react-native-gesture-handler';
+import { ICategories, IRestaurants } from './index';
 
 export const Container = styled.View`
   flex: 1;
@@ -14,7 +15,7 @@ export const ContainerCart = styled.View`
 
 export const TextCart = styled.Text`
   color: #4a4b4d;
-  font-family: 'metropolis.medium';
+  font-family: 'metropolis.bold';
   font-size: 20px;
   text-align: left;
 `;
@@ -50,17 +51,183 @@ export const LocalizationIcon = styled.Image`
   margin-top: 7px;
 `;
 
+export const CategoriesContainer = styled.View`
+  margin-left: 5%;
+  height: 22%;
+`;
+
 export const CategoriesList = styled(
   FlatList as new () => FlatList<ICategories>,
 )``;
 
-export const CategoriesContainer = styled.View`
-  margin-left: 15px;
-`;
-
 export const CategoriesImg = styled.Image`
   width: 88px;
   height: 88px;
-  margin-left: 15px;
+  position: relative;
   border-radius: 10px;
+  margin-bottom: 125px;
+  margin-right: 20px;
 `;
+
+export const CategoriesTitleContainer = styled.View`
+  align-items: center;
+
+  align-self: center;
+  position: absolute;
+  margin-left: 15%;
+`;
+
+export const CategoriesTitle = styled.Text`
+  margin-top: 50px;
+
+  font-family: 'metropolis.bold';
+  font-size: 14px;
+  color: #4a4b4d;
+  text-align: center;
+`;
+
+export const ContainerRestaurants = styled.View`
+  flex-direction: row;
+  margin-left: 6%;
+  margin-bottom: 28px;
+`;
+
+export const RestaurantsText = styled.Text`
+  font-family: 'metropolis.bold';
+  font-size: 18px;
+  color: #4a4b4d;
+  text-align: left;
+`;
+
+export const ExpandButton = styled(RectButton)`
+  margin-left: auto;
+  margin-right: 5%;
+  margin-top: 8px;
+`;
+
+export const ExpandButtonText = styled.Text`
+  font-family: 'metropolis.medium';
+  font-size: 13px;
+  color: #fc6011;
+  text-align: right;
+`;
+
+export const RestaurantsContainer = styled.View`
+  flex: 1;
+`;
+
+export const RestaurantsList = styled(
+  FlatList as new () => FlatList<IRestaurants>,
+)``;
+
+export const RestaurantsImg = styled.Image`
+  width: 100%;
+  height: 193px;
+`;
+
+export const RestaurantsTitleContainer = styled.View`
+  margin-left: 5%;
+  margin-bottom: 5px;
+`;
+
+export const RestaurantsTitle = styled.Text`
+  margin-top: 7px;
+
+  font-family: 'metropolis.extra-bold';
+  font-size: 16px;
+  color: #4a4b4d;
+  text-align: left;
+`;
+
+export const RatingRestaurantContainer = styled.View`
+  flex-direction: row;
+  margin-left: 5%;
+  margin-bottom: 40px;
+`;
+
+export const RatingRestaurantIcon = styled.Image``;
+
+export const RatingRestaurantNumber = styled.Text`
+  font-family: 'metropolis.regular';
+  font-size: 12px;
+  color: #b6b7b7;
+  text-align: left;
+`;
+
+export const PopularContainer = styled.View`
+  flex-direction: row;
+  margin-left: 5%;
+
+  margin-bottom: 40px;
+`;
+
+export const PopularTitle = styled.Text`
+  margin-top: 7px;
+
+  font-family: 'metropolis.extra-bold';
+  font-size: 20px;
+  color: #4a4b4d;
+  text-align: left;
+`;
+
+export const ExpandButtonPopular = styled(RectButton)`
+  margin-left: auto;
+  margin-right: 5%;
+  margin-top: 16px;
+`;
+
+export const ExpandButtonPopularText = styled.Text`
+  font-family: 'metropolis.medium';
+  font-size: 13px;
+  color: #fc6011;
+  text-align: right;
+`;
+
+export const PopularRestaurantsContainer = styled.View`
+  margin-left: 5%;
+  height: 22%;
+`;
+
+export const PopularRestaurantsList = styled(
+  FlatList as new () => FlatList<IRestaurants>,
+)``;
+
+export const PopularRestaurantsImg = styled.Image`
+  width: 228px;
+  height: 135px;
+  position: relative;
+  border-radius: 10px;
+  margin-bottom: 125px;
+  margin-right: 20px;
+`;
+
+export const PopularRestaurantsTitleContainer = styled.View`
+  align-items: center;
+
+  align-self: center;
+  position: absolute;
+`;
+
+export const PopularRestaurantsTitle = styled.Text`
+  margin-top: 140px;
+
+  font-family: 'metropolis.bold';
+  font-size: 18px;
+  color: #4a4b4d;
+  text-align: left;
+`;
+
+export const PopularRatingRestaurantContainer = styled.View`
+  flex-direction: row;
+  margin-left: 5%;
+  margin-bottom: 40px;
+`;
+
+export const PopularRatingRestaurantDescription = styled.Text`
+  font-family: 'metropolis.regular';
+  font-size: 12px;
+  color: #b6b7b7;
+  text-align: left;
+`;
+
+export const PopularRatingRestaurantIcon = styled.Image``;
